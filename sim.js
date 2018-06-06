a35d5e7d778b2645e907183d80b65a1381cab1f3
Isaack Shayo <shayoisaack@gmail.com>
2018-06-06 22:45:15 +0300

    began coding, added plan.txt

---
+++ sim.js
@@ -0,0 +1,9 @@
+var gauss = require('./gaussian-elimination-master/gauss.js');
+//console.log(gauss([[1, 2], [2, 3]], [4, 5]));
+
+var res = {};
+
+function Cell(p){
+	this.p = p;
+	this.poro = poro
+}
