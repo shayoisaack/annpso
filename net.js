63feed26ffb561c2f142a5969c13f8aa1881009c
Isaack Shayo <shayoisaack@gmail.com>
2018-06-15 09:14:10 +0300

    switched to brain.js in order to use linear activation

--- net.js
+++ net.js
@@ -1,5 +1,9 @@
-var brain1 = require('./brain/lib/brain.js');
-var brain = new brain1.NeuralNetwork();
+//var brain1 = require('./brain/lib/brain.js');
+var brain1 = require('brain.js');
+var brain = new brain1.NeuralNetwork({
+  activation: 'sigmoid', // activation function
+  learningRate: 0.6 // global learning rate, useful when training using streams
+});
 var Res = require('./res.js').Res;
 var simulate = require('./simulator.js').simulate;
 
@@ -9,14 +13,15 @@ var examples = [];
 
 //simulate(res, 2, [{loc: 5, p_bh: 1500}]);
 
+var timesteps = 2;
 //generate examples for use in neural network
 for(var i = 0; i < 20; i++){
 	var res = new Res(20);
 	var wells = [{loc: i, p_bh: 1500}];
 	//res.addWells(wells);
 	examples[i] = {};
-	examples[i].input = linearize(res, wells);
-	examples[i].output = [simulate(res, 1, [{loc: i, p_bh: 1500}])];
+	examples[i].input = linearize(res, timesteps, wells);
+	examples[i].output = [simulate(res, timesteps, [{loc: i, p_bh: 1500}])];
 }
 
 //console.log(examples.slice(0,14));
@@ -31,13 +36,13 @@ brain.train(examples, {
 //brain.test({input: examples.input, output: examples.output});
 var res1 = new Res(20);
 var wells1 = [{loc: 18, p_bh: 1500}];
-res1.addWells(wells1)
-console.log('neural network ', brain.run(linearize(res1)));
-console.log('simulator ', simulate(res1, 1, wells1));
+//res1.addWells(wells1)
+console.log('neural network ', brain.run(linearize(res1, timesteps, wells1)));
+console.log('simulator ', simulate(res1, timesteps, wells1));
 
 //console.log(brain);
 
-function linearize(res){
+function linearize(res, time, wells){
 	//linearize res properties from each cell for input to neural net
 	var arr = [];
 	for(var i = 0; i < res.cell.length; i++){
@@ -56,9 +61,12 @@ function linearize(res){
 		var pp = res.cell[i].p*res.cell[i].poro*res.cell[i].So;
 		arr[i] = pp;
 	}
+
+	for(var i = 0; i < wells.length; i++){
+		arr[i] = wells[i].loc;
+		arr[i] = wells[i].p_bh;
+	}
+
+	arr[arr.length] = time;
 	return arr;
-}
-
-function delinearize(){
-
 }
